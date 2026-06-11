6990b55ea44bbaf881630d659f9a9d4cba97e656
Johnson K <johnson@Johnsons-MacBook-Air.local>
2026-06-12 00:03:27 +0530

    added image build workflow

---
+++ terraform-ecs-app-deployment/infra/ecr.tf
@@ -0,0 +1,10 @@
+# # Create ECR repository to store the application Docker Image. 
+# resource "aws_ecr_repository" "sp_ecr_repo" {
+#     name = "sp-ecr-repo"
+#     image_tag_mutability = "MUTABLE"
+# }
+
+
+# output "ecr_repository_url" {
+#     value = aws_ecr_repository.sp_ecr_repo.repository_url
+# }
