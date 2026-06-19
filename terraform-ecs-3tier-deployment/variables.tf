b368a46cf20d13af6917ca37d5417977ded264b6
Johnson K <johnson@Johnsons-MacBook-Air.local>
2026-06-19 13:53:35 +0530

    refactored the infra structure for better dynamic architecture

---
+++ terraform-ecs-3tier-deployment/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "project" {
+    type = string
+    default = "Devops Dojo"
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "prefix" {
+    type = string
+    default = ""
+}
